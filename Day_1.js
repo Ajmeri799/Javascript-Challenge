24442df34635c4c3d8f28750c6ab0165a889a539
Ajmeri Gudiya <149288719+Ajmeri799@users.noreply.github.com>
2024-07-18 23:26:32 +0530

    feat : day one practice code

---
+++ Day_1.js
@@ -0,0 +1,34 @@
+var a = 33;
+console.log(a);
+
+// Declare a variable using let
+
+let b = "ajmeri";
+console.log(b);
+
+// Declare a variable using const
+
+const c = true;
+console.log(c);
+
+// datatype of Oprator
+let num = 23;
+let str = "ajmeri";
+let bool = true;
+let obj = { 1: "a", 2: "b" };
+let arr = [1, 3, 4, 5];
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof obj);
+console.log(typeof arr);
+// reassign value
+let x = 5;
+x = 10;
+console.log(x);
+
+// reassign const values
+const y = 30;
+// y = 25;
+console.log(y);
