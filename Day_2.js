0d2ae82dc6ae6d273d3d2edc1bfee9f4d5bdd5c7
Ajmeri Gudiya <149288719+Ajmeri799@users.noreply.github.com>
2024-07-22 23:47:38 +0530

    feat : java script operators

---
+++ Day_2.js
@@ -0,0 +1,34 @@
+let a = 9;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+//Assignment Oprator
+
+a += 5;
+console.log(a);
+a -= 5;
+console.log(a);
+
+//Comparison  Oprator
+
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a === b);
+
+// Logical Oreator
+
+console.log(a > b && a <= b);
+console.log(a == b || a > b);
+console.log(a != b);
+
+//Ternary Oprator
+console.log("Trnary Oprator");
+console.log(a < b ? "Y" : "N");
